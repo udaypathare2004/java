9498ece3817d23a4ca5dce8973bdc21b8dc6cdf0
udaypathare2004 <udaypathare210@gmail.com>
2025-01-11 14:56:03 +0530

    this is my sec commit

---
+++ AdditionExample.java
@@ -0,0 +1,66 @@
+//public class AdditionExample {
+  //  public static void main(String[] args) {
+    //    int a = 10;
+      //  int b = 20;
+        //int sum = a + b;
+        //System.out.println("Sum: " + sum); // Output: Sum: 3v
+
+       // Write a Java program that subtracts one number from another and prints the result.
+
+//class AdditionExample{
+//   public static void main(String[] args) {
+  //  int a = 60;
+    //int b = 40;
+   // int sub = a - b;
+    //System.err.println("sub :" +sub);
+   
+   // Write a Java program that multiplies two numbers and prints the result
+   //class AdditionExample{
+    //public static void main(String[] args) {
+      //int a = 47;
+       //int b = 56;
+       //int multiplies = a* b;
+       //System.err.println("multiplies :" + multiplies);
+     //
+      // Write a java program to find  largest number among 2
+
+     // import java.util.*;
+      //class AdditionExample{
+        //public static void main(String[] args) {
+          //  int num1 = 20;
+            //int num2 = 40;
+           // if (num1>num2){
+             //   System.out.println("Number one is grater :"+num1);
+           // }
+           // else if  (num2>num1){
+            ////    System.out.println("Number two is grater :"+num2);
+           // }
+            //else
+              //   System.err.println("Numbers are equals");
+       // }
+    
+   // }
+   // write a java program smallest number among 2?
+   //class AdditionExample{
+    //public static void main(String[] args) {
+      //  int num1 = 20;
+        //int num2 = 40;
+        //if (num1<num2){
+          //  System.err.println("Number one  is  greater :"+num1);
+       // }
+        //else if (num2<num1){
+          //  System.err.println("Number two is greater :"+num2);
+       // }
+        //else
+        //System.err.println("numbers are equals");
+    //}
+   //}
+   //write a program whether the character is upper case alphabet or not
+   import java.util.Scanner;
+   class AdditionExample{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a single character");
+        String input =  scanner.nextLine();
+    }
+  }
