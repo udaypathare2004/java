3f7a538768dcfacafc8a7dc0d1dc81d3c35bb29c
udaypathare2004 <udaypathare210@gmail.com>
2025-02-04 23:26:47 +0530

    Add files via upload

---
+++ Ternary.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Ternary{
+    public static void main(String args []){
+        int number = 2 ;
+        String type =((number%2) == 0 ? "even": "odd");
+        System.out.println(type);
+    }
+}
