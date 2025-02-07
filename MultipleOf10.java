7dd9c062a562acbab339ac678abc5fd73c68dd58
udaypathare2004 <udaypathare210@gmail.com>
2025-02-07 19:33:21 +0530

    Add files via upload

---
+++ MultipleOf10.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class MultipleOf10 {
+    public static void main(String args []){
+        Scanner sc = new Scanner(System.in);
+        do{
+            System.out.print("enter your number");
+            int n =sc.nextInt();
+            if(n%10 == 0){
+                break;
+            }
+            System.out.println(n);
+        } while(true);
+    }
+}
