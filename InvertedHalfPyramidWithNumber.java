2e9dc844ec1a587d2f77a0ae72c67e243742618d
udaypathare2004 <udaypathare210@gmail.com>
2025-02-11 09:16:25 +0530

    Add files via upload

---
+++ InvertedHalfPyramidWithNumber.java
@@ -0,0 +1,13 @@
+public class InvertedHalfPyramidWithNumber {
+    public static void invertedhalfpyrmidwithnumbers(int n){
+        for(int i =1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args []){
+        invertedhalfpyrmidwithnumbers(5);
+    }
+}
