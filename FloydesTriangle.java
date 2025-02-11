cf1418e50eb844164d82b0762aa8d8b78e74bd93
udaypathare2004 <udaypathare210@gmail.com>
2025-02-11 09:17:01 +0530

    Add files via upload

---
+++ FloydesTriangle.java
@@ -0,0 +1,16 @@
+public class FloydesTriangle {
+    public static void floydestringle(int n){
+      int  counter=1;
+        for(int i =1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(counter+" ");
+                counter++;
+            }
+            System.out.println();
+        }
+        
+    }
+    public static void main(String args []){
+        floydestringle(5);
+    }
+}
