3e3a4858b3c82fe1f54cd99e9ad55ffff81ce9fd
udaypathare2004 <udaypathare210@gmail.com>
2025-03-07 14:18:27 +0530

    Add files via upload

---
+++ SetBit.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class SetBit {
+public static int setIthbit(int n,int i){
+    int bitmask = 1<<i;
+    return n | bitmask;
+}
+    public static void main(String[] args) {
+        System.out.println(setIthbit(10, 2));
+    }
+}
