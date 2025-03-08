29512f3a0f29a90a408e5780abcaebc7f7f790d3
udaypathare2004 <udaypathare210@gmail.com>
2025-03-08 14:23:31 +0530

    Add files via upload

---
+++ CountSetBit.java
@@ -0,0 +1,17 @@
+public class CountSetBit {
+    public static int countsetbits(int n){
+        int count =0;
+        while (n>0) {
+            if((n & 1) != 0){//check our LSB
+                count++;
+
+            }
+            n = n>>1;
+        }
+        return count;
+    }
+    public static void main(String args[]){
+      System.out.println(countsetbits(10));
+      
+    }
+}
